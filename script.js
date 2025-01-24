924c05328a2c16d49b6ace243ac143e4f6648248
ajkobanpayne <ajkobanpayne@icloud.com>
2025-01-24 12:10:25 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,27 @@
-
 const myWinterCoat = {
-    model: "650 Down Jacket",
-    version: 2.0,
-    color: "black,
-    brand: {
-        name: "REI",
-        website; "https://rei.com",
-        phone: "+18004264840",
-        headquarters: {
-            city: "Sumner",
-            state: "WA",
-            dateFounded: 1938
-        }
-    },,
-    hadHood false,
-    bestUse: ["Casual", "Hiking"],
-    fabric: "Recycled nylon taffeta",
-    liningFabric: "Nylon taffeta",
-    insulated: true
-    insulationType: "down",
-    insulation:	"650-fill-power down", 
-    warmth: "Warmer",
-    weight: {
-        amt 10.8,
-        unit: "ounces"
-    }
-
-}
-
-
-
-
-
+  model: "650 Down Jacket",
+  version: 2.0,
+  color: "black",
+  brand: {
+    name: "REI",
+    website: "https://rei.com",
+    phone: "+18004264840",
+    headquarters: {
+      city: "Sumner",
+      state: "WA",
+      dateFounded: 1938,
+    },
+  },
+  hadHood: false,
+  bestUse: ["Casual", "Hiking"],
+  fabric: "Recycled nylon taffeta",
+  liningFabric: "Nylon taffeta",
+  insulated: true,
+  insulationType: "down",
+  insulation: "650-fill-power down",
+  warmth: "Warmer",
+  weight: {
+    amt: 10.8,
+    unit: "ounces",
+  },
+};
